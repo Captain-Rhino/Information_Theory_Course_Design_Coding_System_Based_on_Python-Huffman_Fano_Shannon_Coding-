41730068ea07593054fa46ae7b100f12153dfe74
Captain_Rhino <1652136518@qq.com>
2024-05-15 00:23:21 +0800

    初始化PyQt

--- hello.py
+++ hello.py
@@ -19,6 +19,9 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+
+
+
         self.pushButton.setGeometry(QtCore.QRect(90, 180, 161, 81))
         self.pushButton.setObjectName("pushButton")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
@@ -39,12 +42,22 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        self.pushButton.clicked.connect(self.on_pushButton_clicked)
+
+        self.pushButton_2.clicked.connect(self.on_pushButton_2_clicked)
+    def on_pushButton_clicked(self):
+        print('button1 is pushed')
+
+    def on_pushButton_2_clicked(self):
+        print('button2 is pushed')
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "按键1"))
         self.pushButton_2.setText(_translate("MainWindow", "按键2"))
         self.menuHello_World.setTitle(_translate("MainWindow", "Hello World"))
+
 
 if __name__ == '__main__':
     import sys
